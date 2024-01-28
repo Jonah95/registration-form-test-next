b09eea7fc1d8ef546a60f0e916dd3834852e7ff4
Jonah95 <92964047+Jonah95@users.noreply.github.com>
2024-01-29 00:20:08 +0530

    Add files via upload

---
+++ app/register/imageData.js
@@ -0,0 +1,123 @@
+
+
+   const ImageData = [ 
+    {
+      id: 1,
+      src: './img-1.webp',
+      title: 'Vision of India',
+      details: 'In this poignant image, the eye reflects not just the colors of the Indian flag but also a myriad of emotions – pride, nostalgia, and a profound sense of belonging, creating a visual narrative that resonates with the heartstrings of patriotism.',
+      artist: 'Gokul',
+      rawl: 'https://1drv.ms/i/s!AorqtJnxB5npgdx3DGVnmOLVI3KJHQ?e=pbutih',
+    },
+    {
+      id:2,
+      src: './img-2.webp',
+      title: 'Hues of Unity',
+      details: `Dive into 'Hues of Unity,' a vibrant photocollage fusing a warm sunset silhouette with a bird, serene white expanse, and vivid green peacock scene. A visual ode to India's rich tapestry of nature and national pride.`,
+      artist: 'Akash Ram',
+      rawl: 'https://1drv.ms/i/s!AorqtJnxB5npgdx2Uy_SHsCeVZvIMw?e=KUZQ6P',
+    },
+    {
+      id:3,
+      src: './img-3.webp',
+      title: 'Individual Property',
+      details: `Captured the essence of bird's architecture through my lens. See how safely it has built.`,
+      artist: 'Arun Kumar.K',
+      rawl: 'https://1drv.ms/i/s!AorqtJnxB5npgdx42Kk0XPtNAsbEgA?e=Rr61QE',
+    },
+    {
+      id:4,
+      src: './img-4.webp',
+      title: 'Gentle Giant in Hues',
+      details: `The canvas comes alive with vibrant strokes that capture the essence of these gentle giants.`,
+      artist: 'Arun Kumar.K',
+      rawl: 'https://1drv.ms/i/s!AorqtJnxB5npgdx5paL8g4HxCcfCFw?e=U7PAu4',
+    },
+    {
+      id:5,
+      src: './img-5.webp',
+      title: 'Poverty',
+      details: `Rich get richer, poor get poorer`,
+      artist: 'Jeyasudhan Amirthavasagam',
+      rawl: 'https://1drv.ms/i/s!AorqtJnxB5npgdx6Cz-9VIvxNHBNyg?e=pUGHwe',
+    },
+    {
+      id:6,
+      src: './img-6.webp',
+      title: 'Feed the Needy',
+      details: `The war against hunger is truly mankind's war of liberation. -John F.Kennedy`,
+      artist: 'Murali Krishnan',
+      rawl: 'https://1drv.ms/i/s!AorqtJnxB5npgdx7eM8T8jdEm32G-g?e=lXKIGm',
+    },
+    {
+      id:7,
+      src: './img-7.webp',
+      title: 'Leather Art',
+      details: `Leather Art: This artwork symbolizes the fusion of India’s traditional dance form Bharatanatyam and India’s technological prowess “Mission ADITYA - L1” (Exploration of the sun’s Mysteries).
+      Bharatanatyam: Bharatanatyam a traditional Indian dance form, conveys messages through a combination of hand gestures (mudras), facial expressions, body movements.
+      Mission Aditya -L1: Aditya -L1 is a spacecraft for studying the solar atmosphere, designed and developed by the Indian Space Research Organisation (ISRO). Aditya -L1 Successfully Accomplished “Halo-Orbit Insertion (HOI) of its solar observatory spacecraft on January 6, 2024 (IST).`,
+      artist: 'Hariharan Veluchami',
+      rawl: 'https://1drv.ms/i/s!AorqtJnxB5npgdx8al2svAjoioGXzQ?e=W8eaov',
+    },
+    {
+      id:8,
+      src: './img-8.webp',
+      title: 'Farming Forward',
+      details: `"If you tickle the earth with a hoe she laughs with a harvest" - Douglas Jerrold`,
+      artist: 'Jeya Sundari G',
+      rawl: 'https://1drv.ms/i/s!AorqtJnxB5npgdx9gQ4p-IM4Xrt4Sg?e=Gj7Qym',
+    },
+    {
+      id:9,
+      src: './img-9.webp',
+      title: 'When Nature Makes TRIRANGA!',
+      details: `Freedom lies in nature, it shows us how to live, love and grow alongside each other. A random click of nature making triranga!`,
+      artist: 'Haritha',
+      rawl: 'https://1drv.ms/i/s!AorqtJnxB5npgdx_7K6fGRCNTnr4og?e=7seeP1',
+    },
+    {
+      id:10,
+      src: './img-10.webp',
+      title: 'The true republic',
+      details: `“men, their rights, and nothing more;
+      Women, their rights, and nothing less.”`,
+      artist: 'Susan B.Anthony',
+      rawl: 'https://1drv.ms/i/s!AorqtJnxB5npgd0A1t94Bdyeg8GUkQ?e=ul1TSP',
+    },
+    {
+      id:11,
+      src: './img-11.webp',
+      title: 'Three Women',
+      details: `The Saffron colour represents strength and courage, The White colour women represent peace and truth aling with the dharma chakra, The green colour women represent fertility, growth and auspiciousness of the land.`,
+      artist: 'Vignesh Sugavanaeswaran',
+      rawl: 'https://1drv.ms/i/s!AorqtJnxB5npgd0Bdk6NvLiytE3EjQ?e=G7VaNV',
+    },
+    {
+      id:12,
+      src: './img-12.webp',
+      title: 'Anbe Shivam',
+      details: 'Be brave in the Face of Fear - Lord Shiva',
+      artist: 'Umashankar',
+      rawl: 'https://1drv.ms/i/s!AorqtJnxB5npgd0CGyS-BsLb5tgD4g?e=6ONcrk',
+    },
+    {
+      id:13,
+      src: './img-13.webp',
+      title: 'Ephemeral Majesty',
+      details: `A mesmerizing close-up capture of a dragonfly in flight, its iridescent wings shimmering with ethereal hues as it dances gracefully amidst the golden glow of a sunlit meadow. Witness the fleeting beauty of nature's aerial acrobat frozen in a moment of delicate splendor.`,
+      artist: 'Keerthinivasan Krishnamurthy',
+      rawl: 'https://1drv.ms/i/s!AorqtJnxB5npgd0DIS5WOnvNuAOZ8w?e=Ada36w',
+    },
+    {
+      id:14,
+      src: './img-14.webp',
+      title: 'Harvest',
+      details: `The farmer has patience and trusts the process`,
+      artist: 'Kamesh',
+      rawl: 'https://1drv.ms/i/s!AorqtJnxB5npgdx-K2Aj6jC4pl1mNA?e=JUXbgH',
+    },
+
+
+   ];
+
+  export default ImageData;
